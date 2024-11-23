450b3e6ad06f1379398db46c52c79e7e2813fcb4
William <ywwwtseng@gmail.com>
2024-11-23 22:17:06 +0800

    feat: url stringify search parameters func

---
+++ packages/url/index.js
@@ -0,0 +1,6 @@
+export const stringify = (params) => {
+  return Object.entries(params)
+    .filter(([_, value]) => value !== null && value !== '' && value !== undefined) // 过滤掉 null, '', undefined
+    .map(([key, value]) => `${key}=${value}`) // 拼接成 key=value
+    .join('&'); // 使用 & 连接
+};
